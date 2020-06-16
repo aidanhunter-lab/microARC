ddd62e4503d2a3c929c544e42ee1788888184ac3
aidan <Aidan.Hunter@soton.ac.uk>
2020-06-16 11:24:40 +0100

    omit extra output when integrating

--- costFun.m
+++ costFun.m
@@ -12,7 +12,7 @@ nTraj = Forc.nTraj;
 Params = updateParameters2(Params, FixedParams, x);
 
 % Integrate model
-[OUT, AUXVARS, RATES, namesExtra, nExtra] = ... 
+[OUT, AUXVARS, AUXVARS_2d, namesExtra, nExtra] = ... 
     integrateTrajectories(FixedParams, Params, Forc, v0, ode45options);
 
 % Extract solutions
@@ -21,28 +21,15 @@ out.P = reshape(OUT(FixedParams.PP_index,:,:), [nPP nz nt nTraj]);
 out.Z = reshape(OUT(FixedParams.ZP_index,:,:), [1 nz nt nTraj]);
 out.OM = reshape(OUT(FixedParams.OM_index,:,:), [1 nz nt nTraj]);
 
-returnExtras = FixedParams.returnExtras;
-if ~strcmp(returnExtras, 'none')
-    switch returnExtras
-        case 'auxiliary'
-            for k = 1:nExtra
-                auxVars.(namesExtra{k}) = squeeze(AUXVARS(:,k,:,:));
-            end
-        case 'auxiliaryAndRates'
-            for k = 1:nExtra
-                auxVars.(namesExtra{k}) = squeeze(AUXVARS(:,k,:,:));
-            end
-            rates.N = reshape(RATES(FixedParams.IN_index,:,:), [1 nz nt nTraj]);
-            rates.P = reshape(RATES(FixedParams.PP_index,:,:), [nPP nz nt nTraj]);
-            rates.Z = reshape(RATES(FixedParams.ZP_index,:,:), [1 nz nt nTraj]);
-            rates.OM = reshape(RATES(FixedParams.OM_index,:,:), [1 nz nt nTraj]);
-        case 'rates'
-            rates.N = reshape(RATES(FixedParams.IN_index,:,:), [1 nz nt nTraj]);
-            rates.P = reshape(RATES(FixedParams.PP_index,:,:), [nPP nz nt nTraj]);
-            rates.Z = reshape(RATES(FixedParams.ZP_index,:,:), [1 nz nt nTraj]);
-            rates.OM = reshape(RATES(FixedParams.OM_index,:,:), [1 nz nt nTraj]);
+if sum(nExtra) > 0
+    for k = 1:nExtra(1)
+        auxVars.(namesExtra{k}) = squeeze(AUXVARS(:,k,:,:));
+    end
+    for k = 1:nExtra(2)
+        auxVars.(namesExtra{k+nExtra(1)}) = squeeze(AUXVARS_2d(:,:,k,:,:));
     end
 end
+
 
 
 % Compare model to observations
